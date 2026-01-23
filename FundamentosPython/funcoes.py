f019e88773b92532c0098f133b426d926192b1ae
Miguel Mantoan Castellani <miguelmcastell@hotmail.com>
2026-01-22 21:06:36 -0300

    Modularizacao

---
+++ FundamentosPython/funcoes.py
@@ -0,0 +1,5 @@
+def saudacao (nome):
+    print(f'Olá {nome}, vamos estudar IA?')
+
+def soma(a, b):
+    return a + b
---
+++ FundamentosPython/main.py
@@ -0,0 +1,4 @@
+from funcoes import saudacao, soma
+
+saudacao('Miguel')
+print(soma(2, 10))
